ed37766991c16e9ceb64779ab8118b0a72d18a0c
nauel <89044685+NauelSerraino@users.noreply.github.com>
2025-01-04 19:41:17 +0100

    added useless punctuation to stopwords

--- develop/pipe/03_train_emb.py
+++ develop/pipe/03_train_emb.py
@@ -1,3 +1,19 @@
 """
 Train embeddings using the TWEC model.
 """
+import os
+from twec.twec import TWEC
+from gensim.models.word2vec import Word2Vec
+
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.join(__file__, "../.."))))
+
+from develop.utils.paths import DATA
+
+aligner = TWEC(size=100, siter=10, diter=10, workers=5)
+
+
+aligner.train_compass(
+    os.path.join(DATA, "02_preprocessed_corpus"),
+    overwrite=False,
+    ) 
