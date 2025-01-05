0fbb0e3aed756d909e4f906b5cd6b83bbd2142df
nauel <89044685+NauelSerraino@users.noreply.github.com>
2025-01-05 23:21:54 +0100

    pipe update: add sliced embedding and word freq

--- develop/pipe/03_train_emb.py
+++ develop/pipe/03_train_emb.py
@@ -9,11 +9,15 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.join(__file__, "../.."))))
 
 from develop.utils.paths import DATA
+input_dir = os.path.join(DATA, "02_preprocessed_corpus")
 
 aligner = TWEC(size=100, siter=10, diter=10, workers=5)
 
 
-aligner.train_compass(
-    os.path.join(DATA, "02_preprocessed_corpus"),
-    overwrite=False,
-    ) 
+if __name__ == "__main__": 
+    aligner.train_compass(input_dir, overwrite=False)
+
+    for file in os.listdir(input_dir):
+        breakpoint()
+        input_file = os.path.join(input_dir, file)
+        aligner.train_slice(input_file, save=True)
