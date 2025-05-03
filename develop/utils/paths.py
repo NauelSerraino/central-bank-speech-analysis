1ca28ab70f2776737bc4c64f14d8d603570471d7
nauel <89044685+NauelSerraino@users.noreply.github.com>
2025-05-03 15:08:40 +0200

    refactor(develop): update paths file

--- develop/utils/paths.py
+++ develop/utils/paths.py
@@ -3,13 +3,13 @@ import os
 base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 DATA = os.path.join(base_dir, 'data')
+DATA_ALT = os.path.join(base_dir, 'data_alt')
 DEVELOP = os.path.join(base_dir, 'develop')
 MODEL = os.path.join(base_dir, 'model')
+DEV_LOGS = os.path.join(base_dir, 'develop', 'logs')
 
 os.makedirs(DATA, exist_ok=True)
+os.makedirs(DATA_ALT, exist_ok=True)
 os.makedirs(DEVELOP, exist_ok=True)
 os.makedirs(MODEL, exist_ok=True)
-
-print(f"Data path: {DATA}")
-print(f"Develop path: {DEVELOP}")
-print(f"Model path: {MODEL}")
+os.makedirs(DEV_LOGS, exist_ok=True)
