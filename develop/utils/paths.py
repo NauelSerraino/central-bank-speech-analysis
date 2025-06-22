5d5620c223568eb2b8fd367ee6afa589ce5419a0
nauel <89044685+NauelSerraino@users.noreply.github.com>
2025-06-22 17:58:43 +0200

    analysis(notebooks): first experiments with HMM

--- develop/utils/paths.py
+++ develop/utils/paths.py
@@ -4,11 +4,13 @@ base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 DATA = os.path.join(base_dir, 'data')
 DATA_ALT = os.path.join(base_dir, 'data_alt')
+NOTEBOOKS_ANALYSIS = os.path.join(DATA_ALT, 'notebooks_analysis')
 DEVELOP = os.path.join(base_dir, 'develop')
 MODEL = os.path.join(base_dir, 'model')
 DEV_LOGS = os.path.join(base_dir, 'develop', 'logs')
 
 os.makedirs(DATA, exist_ok=True)
+os.makedirs(NOTEBOOKS_ANALYSIS, exist_ok=True)
 os.makedirs(DATA_ALT, exist_ok=True)
 os.makedirs(DEVELOP, exist_ok=True)
 os.makedirs(MODEL, exist_ok=True)
